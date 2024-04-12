b18484e679b3d044adef728b0f2fdb37df520a6e
Gokulcoder-99 <gokulkrishnan072@gmail.com>
2024-04-12 21:45:18 +0530

    check3

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const env = require('dotenv');
+const cors = require("cors");
+const env = require("dotenv");
 const connectDB = require("./Db/db");
-const authroute = require('./route/authroute');
-const vaildCheck = require('./middleware/validCheck');
-const userRoute = require('./route/userRoute');
+const authroute = require("./route/authroute");
+const vaildCheck = require("./middleware/validCheck");
+const userRoute = require("./route/userRoute");
 
 app.use(express.json());
-   env.config();
+                  env.config();
 app.use(cors());
 
-app.use('/api/auth',authroute)
-app.use('/api/user',vaildCheck,userRoute);
+app.use("/api/auth", authroute);
+app.use("/api/user", vaildCheck, userRoute);
 
-const port = process.env.PORT
-const server = async()=>{
-    try{
-       await connectDB()
-        app.listen(port,()=>{
-            console.log(`${port} is connected.`)
-        })
-    }catch(err){
-            console.log(err,'server is not running')
-    }
-}
+const port = process.env.PORT;
+const server = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`${port} is connected.`);
+    });
+  } catch (err) {
+    console.log(err, "server is not running");
+  }
+};
 server();
