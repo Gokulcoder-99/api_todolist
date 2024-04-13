7606b9e19839aa98d38b7a60e92fb6299c49d394
Gokulcoder-99 <gokulkrishnan072@gmail.com>
2024-04-13 16:06:17 +0530

    2

--- Db/db.js
+++ Db/db.js
@@ -7,22 +7,23 @@ const pool = new Pool({
   password: "gokul",
   port: 5432,
 });
-const createTablesQuery = `
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
-CREATE TABLE IF NOT EXISTS todos (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  task VARCHAR(255) NOT NULL,
-  completed BOOLEAN DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+const createTablesQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    task VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
 `;
 
 // Function to execute the SQL commands to create the tables
@@ -39,7 +40,6 @@ async function createTables() {
 createTables();
 
 // Create User function
-
 async function createUser(name, email, password) {
   const query = {
     text: "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
@@ -51,12 +51,10 @@ async function createUser(name, email, password) {
     return result.rows[0];
   } catch (error) {
     console.error("Error creating user:", error);
-    throw error;
   }
 }
 
 // Get specific user
-
 async function getUserByEmail(email) {
   const query = {
     text: "SELECT * FROM users WHERE email = $1",
@@ -72,7 +70,6 @@ async function getUserByEmail(email) {
 }
 
 // Create Todo
-
 async function createTodo(userId, task) {
   const query = {
     text: "INSERT INTO todos(user_id, task) VALUES($1, $2) RETURNING *",
@@ -88,7 +85,6 @@ async function createTodo(userId, task) {
 }
 
 // Get all todo relate to user
-
 async function getTodosByUserId(userId) {
   const query = {
     text: "SELECT * FROM todos WHERE user_id = $1",
@@ -103,8 +99,7 @@ async function getTodosByUserId(userId) {
   }
 }
 
-// update task
-
+// Update task
 async function updateTodo(todoId, updatedTask, completed) {
   const query = {
     text: "UPDATE todos SET task = $1, completed = $2 WHERE id = $3 RETURNING *",
@@ -119,8 +114,7 @@ async function updateTodo(todoId, updatedTask, completed) {
   }
 }
 
-// delete task
-
+// Delete task
 async function deleteTodo(todoId) {
   const query = {
     text: "DELETE FROM todos WHERE id = $1 RETURNING *",
